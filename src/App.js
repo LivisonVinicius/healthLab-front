0368cc4e6363bfb0a3143496dfd1589fbc7343b4
LivisonVinicius <livisongc@gmail.com>
2022-09-28 20:47:44 -0300

    Initial setup

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import { Suspense, lazy, Component } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+const Loading = () => <div>Loading...</div>;
+
+const LazyWrapper = (Component) => (props) =>
+  (
+    <Suspense fallback={<Loading />}>
+      <Component {...props} />
+    </Suspense>
+  );
+
+const InitialLoad = LazyWrapper(lazy(() => import("./pages/InitialLoad")));
+// const Landing = LazyWrapper(lazy(() => import("./pages/Landing")));
+// const Login = LazyWrapper(lazy(() => import("./pages/Login")));
+// const Register = LazyWrapper(lazy(() => import("./pages/Register")));
+
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<InitialLoad />}>
+          {/* <Route path="/welcome" element={<Landing />} /> */}
+          {/* <Route path="/signin" element={<Login />} /> */}
+          {/* <Route path="/signup" element={<Register />} /> */}
+          {/* <Route path="*" element={<div>Not found!</div>} /> */}
+        </Route>
+      </Routes>
+    </Router>
+  );
+}
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+import "./assets/style/reset.css";
+import "./assets/style/style.css";
+
+ReactDOM.render(<App />, document.querySelector(".root"));
