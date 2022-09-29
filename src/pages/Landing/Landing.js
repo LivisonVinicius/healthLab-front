91163175ddd90012284146a6fcfa64a948f08912
LivisonVinicius <livisongc@gmail.com>
2022-09-29 01:09:21 -0300

    Refactor: styled components from landing moved to styled components folder

--- src/pages/Landing/Landing.js
+++ src/pages/Landing/Landing.js
@@ -1,8 +1,7 @@
-import styled from "styled-components";
 import BGImage from "../../assets/images/benefits-banner 1.png";
 import logo from "../../assets/images/initialLoadLogo.png";
 import { useNavigate } from "react-router-dom";
-import colors from "../../config/colors";
+import { Container } from "../../styledComponents/Landing/LandingSC";
 
 export default function Landing() {
   const navigate = useNavigate();
@@ -27,66 +26,3 @@ export default function Landing() {
     </Container>
   );
 }
-
-const Container = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
-  height: 100vh;
-  background: url("${(props) => props.bg}") 50% 50% no-repeat;
-  background-size: cover;
-  color: ${colors.white};
-  > div {
-    width: 100%;
-    height: 100%;
-    background: rgba(31, 31, 31, 0.5);
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    img {
-      position: absolute;
-      width: 400px;
-      height: 400px;
-      z-index: 1;
-      left: 0px;
-      top: -80px;
-    }
-    h1 {
-      font-size: 48px;
-      line-height: 49px;
-      text-align: right;
-      width: 220px;
-      height: 199px;
-      position: absolute;
-      right: 32px;
-      bottom: 35%;
-    }
-    div {
-      width: 100%;
-      height: 120px;
-      position: absolute;
-      left: 15%;
-      bottom: 80px;
-    }
-  }
-  .button {
-    width: 70%;
-    height: 50px;
-    font-size: 16px;
-    line-height: 16px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    text-align: center;
-    color: ${colors.white};
-    border-radius: 50px;
-    margin-bottom: 10px;
-  }
-  .primary {
-    background-color: ${colors.primary};
-  }
-  .secondary {
-    background-color: ${colors.secondary};
-  }
-`;
---
+++ src/styledComponents/Landing/LandingSC.js
@@ -0,0 +1,65 @@
+import colors from "../../config/colors";
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100vh;
+  background: url("${(props) => props.bg}") 50% 50% no-repeat;
+  background-size: cover;
+  color: ${colors.white};
+  > div {
+    width: 100%;
+    height: 100%;
+    background: rgba(31, 31, 31, 0.5);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    img {
+      position: absolute;
+      width: 400px;
+      height: 400px;
+      z-index: 1;
+      left: 0px;
+      top: -80px;
+    }
+    h1 {
+      font-size: 48px;
+      line-height: 49px;
+      text-align: right;
+      width: 220px;
+      height: 199px;
+      position: absolute;
+      right: 32px;
+      bottom: 35%;
+    }
+    div {
+      width: 100%;
+      height: 120px;
+      position: absolute;
+      left: 15%;
+      bottom: 80px;
+    }
+  }
+  .button {
+    width: 70%;
+    height: 50px;
+    font-size: 16px;
+    line-height: 16px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    color: ${colors.white};
+    border-radius: 50px;
+    margin-bottom: 10px;
+  }
+  .primary {
+    background-color: ${colors.primary};
+  }
+  .secondary {
+    background-color: ${colors.secondary};
+  }
+`;
